684a8aa6033d75fe94ea87919659a489016e3ce9
Lamperkat <elnur.zhubanov@gmail.com>
2024-09-03 23:49:18 +0500

    fix:carousel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ['./*.{html,js}'],
   theme: {
     fontSize: {
-      xs: ["12px", "16.8px"],
-      sm: ["14px", "19.6px"],
-      base: ["16px", "22.4px"],
-      lg: ["22px", "30.8px"],
-      xl: ["24px", "32px"],
+      xs: ['12px', '16.8px'],
+      sm: ['14px', '19.6px'],
+      base: ['16px', '22.4px'],
+      lg: ['22px', '30.8px'],
+      xl: ['24px', '32px'],
     },
     screens: {
-      xs: "480px",
-      md: "1200px",
+      mobile: { max: '480px' },
+      tablet:{
+        min:'481px',
+        max:'992px'
+      },
+      desktop: { min: '1200px' },
     },
 
     extend: {
-      fontFamily:{
-  ubuntu: ['Ubuntu', 'sans-serif'],
+      fontFamily: {
+        ubuntu: ['Ubuntu', 'sans-serif'],
       },
       screens: {
-        "max-xs": { max: "480px" },
+        xs: '480px',
+        md: '1200px',
+      
       },
       colors: {
-        lightRed: "#FFEFEE",
-        error: "#ED3A32",
-        midGray: "#D1D4D7",
-        primary: "#008F91",
-        secondary: "#F05E22",
+        dark: '#333839',
+        lightRed: '#FFEFEE',
+        error: '#ED3A32',
+        midGray: '#D1D4D7',
+        primary: '#008F91',
+        secondary: '#F05E22',
       },
     },
   },
+
   plugins: [],
 };
